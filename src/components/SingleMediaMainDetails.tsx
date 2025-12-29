4e104d3b1e2e7d2181574f0250edb3b5967cad52
tokyoprikol <eugene19190@gmail.com>
2025-12-29 14:43:09 +0100

    Create SingleMediaMainDetails.tsx component. Add types.

---
+++ src/components/SingleMediaMainDetails.tsx
@@ -0,0 +1,71 @@
+import { getPoster } from "../utils/tmdb.ts";
+import type { ContentRating, MediaItem } from "../types.ts";
+
+interface DetailsProps {
+    media: MediaItem;
+    usContRat: ContentRating | undefined;
+}
+
+const SingleMediaMainDetails = ({ media, usContRat }: DetailsProps) => {
+    return (
+        <div className="relative w-full overflow-hidden">
+            <img
+                src={getPoster(media.backdrop_path, "original")}
+                alt="Backdrop"
+                className="absolute inset-0 h-full w-full object-cover"
+            />
+
+            <div className="absolute inset-0 bg-linear-to-b from-black/50 to-black/90" />
+
+            <div className="relative flex items-start gap-8 px-15 py-10 text-white">
+                <img
+                    className="w-80 shadow-2xl"
+                    src={getPoster(media.poster_path, "original")}
+                    alt="poster"
+                />
+                <div>
+                    <div className="text-4xl">
+                        <span className="font-semibold">
+                            {media.title || media.name}
+                        </span>
+                        <span className="ml-2 text-neutral-300">
+                            ({media.first_air_date?.slice(0, 4)})
+                        </span>
+                    </div>
+                    <div className="mt-2 flex items-center">
+                        <span className="rounded-sm border border-neutral-400 p-1 text-sm text-neutral-400">
+                            {usContRat?.rating}
+                        </span>
+                        <div className="ml-2">
+                            {media.genres?.map((g) => g.name).join(", ")}
+                        </div>
+                    </div>
+                    <div className="mt-10 text-neutral-300 italic">
+                        {media.tagline}
+                    </div>
+                    <div className="mt-10">
+                        <div className="mb-2 font-semibold">Overview</div>
+                        <span className="text-sm text-neutral-200">
+                            {media.overview}
+                        </span>
+                    </div>
+                    <div className="mt-10">
+                        <span>Created By:</span>
+                        <div className="mt-4 flex gap-5">
+                            {media.created_by?.map((c) => (
+                                <div
+                                    key={c.id}
+                                    className="cursor-pointer border-b font-semibold"
+                                >
+                                    {c.name}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleMediaMainDetails;
--- src/pages/SingleMediaPage.tsx
+++ src/pages/SingleMediaPage.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useFetching } from "../hooks/useFetching";
+
 import { tvService } from "../API/tvService";
 import { movieService } from "../API/movieService";
+
 import ErrorMes from "../components/ErrorMes";
-import { getPoster } from "../utils/tmdb";
-import { Loader } from "lucide-react";
-import type { MediaItem, ContentRating, Cast, Review } from "../types";
 import ReviewList from "../components/ReviewList.tsx";
 import ActorList from "../components/ActorList.tsx";
+import SingleMediaMainDetails from "../components/SingleMediaMainDetails.tsx";
+
+import { Loader } from "lucide-react";
+import type { MediaItem, ContentRating, Cast, Review } from "../types";
 
 const SingleMediaPage = () => {
     const { mediaType, id } = useParams();
@@ -61,63 +64,7 @@ const SingleMediaPage = () => {
         <h1>Media not found</h1>
     ) : (
         <div>
-            <div className="relative w-full overflow-hidden">
-                <img
-                    src={getPoster(media.backdrop_path, "original")}
-                    alt="Backdrop"
-                    className="absolute inset-0 h-full w-full object-cover"
-                />
-
-                <div className="absolute inset-0 bg-linear-to-b from-black/50 to-black/90" />
-
-                <div className="relative flex items-start gap-8 px-15 py-10 text-white">
-                    <img
-                        className="w-80 shadow-2xl"
-                        src={getPoster(media.poster_path, "original")}
-                        alt="poster"
-                    />
-                    <div>
-                        <div className="text-4xl">
-                            <span className="font-semibold">
-                                {media.title || media.name}
-                            </span>
-                            <span className="ml-2 text-neutral-300">
-                                ({media.first_air_date?.slice(0, 4)})
-                            </span>
-                        </div>
-                        <div className="mt-2 flex items-center">
-                            <span className="rounded-sm border border-neutral-400 p-1 text-sm text-neutral-400">
-                                {usContRat?.rating}
-                            </span>
-                            <div className="ml-2">
-                                {media.genres?.map((g) => g.name).join(", ")}
-                            </div>
-                        </div>
-                        <div className="mt-10 text-neutral-300 italic">
-                            {media.tagline}
-                        </div>
-                        <div className="mt-10">
-                            <div className="mb-2 font-semibold">Overview</div>
-                            <span className="text-sm text-neutral-200">
-                                {media.overview}
-                            </span>
-                        </div>
-                        <div className="mt-10">
-                            <span>Created By:</span>
-                            <div className="mt-4 flex gap-5">
-                                {media.created_by?.map((c) => (
-                                    <div
-                                        key={c.id}
-                                        className="cursor-pointer border-b font-semibold"
-                                    >
-                                        {c.name}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <SingleMediaMainDetails media={media} usContRat={usContRat} />
             <ActorList cast={cast} />
             <ReviewList reviews={reviews} />
         </div>
